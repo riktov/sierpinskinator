5f74f758f3bf0842b8b40cc5ea38642ade042205
Paul Richter <nobuyuki.richter@gmail.com>
2023-04-30 16:22:39 +0900

    rename cddb.js to input.js

--- input.js
+++ input.js
@@ -1,48 +1,46 @@
-/** Takes a 8-character hex string and generates state and color grids.
- * The input string is assumed to be a cddb id, so there are some bits that
- * rarely or never change. We minimize their effect by using them as the least
- * signigicant bits in the colors.
- */
-function cddbToGrid(cddb) {
-    //convert a 32-bit cddb string to a 9-bits of state and 15 bits of color.
-    //a state of True means filled, not recursing
-    var states8 = parseInt(cddb.substring(0, 2), 16) ;  //8 bits, track time checksum
-    var states1 = parseInt(cddb.substring(5, 6), 16) % 2 ;  //1 bit, last bit of total time
-    var statesBits = (states8 << 1) + states1 ;   //append the bit
+function rearrangeCddbString(cddbStr) {
+    /* rearrange a CDDB string to place the least random digits at the front.*/
+    let checkSum     = cddbStr.substring(0, 2) ;    //reliably random
+    let trackTime1   = cddbStr.substring(2, 3) ; //almost always 0, sometimes 1 (over 68 minutes), never greater
+    let trackTime234 = cddbStr.substring(3, 6) ; // reliably random towards the tail
+    let numTracks    = cddbStr.substring(6) ;    //first digit usually 0, second usually between 8 and d?
 
-    //we only need as many colors as there are non-recursing cells
+    return trackTime1 + numTracks + trackTime234 + checkSum ;
+}
+
+function int32ToGrid(num32) {
+    //convert a 32-bit integer to a 3x3 grids of state and color.
+    //values are taken from the tail end.
+    //a state of True means recursing, not filled with solid color
+
     var numFilled = 0 ;
 
     var states = [] ;
 
     for(var i = 0 ; i < 9 ; i++) {
-        stateval = ((statesBits % 2) == 1) ;   //last bit
-        states[i] = stateval ;
-        if(stateval) {
+        var isRecursing = ((num32 % 2) == 1) ;   //last bit
+        states[i] = isRecursing ;
+        if(!isRecursing) {
             numFilled++ ;
         }
-        statesBits >>= 1 ;
+        num32 >>= 1 ;
     }
 
     //There is a 1/512 chance that all the state bits will recursing
     //check for that, and in that case invert one of them
     //all non-recursing is boring but will display
     if(numFilled == 0) {
-        statesBits[4] = !statesBits[4] ;
+        states[4] = !states[4] ;
         numFilled = 1;
     }
 
+    //We only need as many colors as there are non-recursing cells,
+    //so we allot all remaining bits among the colors. The fewer colors needed,
+    //the more possible values for those colors.
+
     var colorPalette = [] ;
 
-    //15 bits, total time. First is almost always 0, 1 if over 68 minutes.
-    var colors15 = parseInt(cddb.substring(2, 6), 16) >> 1 ;   
-    //we should pick off the LSBs and feed them to the front of the three colors
-
-    //last 8 bits, number of tracks 
-    //usually close to 15, not random enough
-    var last8 = parseInt(cddb.substring(6), 16) ;  
-
-    //extend the palette to the number of filled cells
+    //initialize the palette to the number of filled cells
     for(var i = 0 ; i < numFilled ; i++) {
         colorPalette[i] = 0 ;
     }
@@ -50,20 +48,18 @@ function cddbToGrid(cddb) {
     var idx = 0 ;
 
     //deal the bits off the tail to the palette trays
-    for(var i = 0 ; i < 15 ; i++) {
+    for(var i = 0 ; i < 23 ; i++) {
         idx = i % numFilled ;
-        let lsb = colors15 % 2 ;
+        let bit = num32 % 2 ;
 
-        colorPalette[idx] = (colorPalette[idx] << 1) + lsb ;
-        colors15 >>= 1 ;        
+        colorPalette[idx] = (colorPalette[idx] << 1) + bit ;
+        num32 >>= 1 ;        
     }
-
-    console.log(colorPalette) ;
 
     var colors = [] ;
 
     for(var i = 0 ; i < 9 ; i++) {
-        if(states[i]) {
+        if(!states[i]) {
             colors[i] = intToRGB(colorPalette.pop()) ;
         } else {
             colors[i] = "#000000" ;
@@ -72,11 +68,6 @@ function cddbToGrid(cddb) {
 
     console.log(colors) ;
     
-    //temporary hack - reverse the states, because is everything is coming out black and white
-    for(var i = 0; i < 9 ; i++) {
-        states[i] = !states[i] ;
-    }
-
     let slicedStates = [
         states.slice(0, 3),
         states.slice(3, 6),
@@ -96,12 +87,19 @@ function cddbToGrid(cddb) {
 }
 
 function intToRGB(n) {
-    //convert an integer of any size to a 24-bit color, 
-    //round-robin the least significant bits to the the three channels
+    //convert an integer of any size up to 24 bits to a
+    //string representation of 24-bit color
+    
+    //replace black with something nicer
+    if(n == 0) {
+        return "#de2bef" ;
+    }
+
     var cols = [
         0,0,0
     ]
-
+    
+    //Deal out the bits from the tail to the the three channels
     for(var i = 0 ; i < 24; i++) {
         let bit = n % 2 ;
         let idx = i % 3 ;
